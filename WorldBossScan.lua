5334176d476f7bb8d2d4b84822be0864a430facb
jcarlyle93 <j.j.carlyle93@gmail.com>
2025-01-16 12:02:56 +0000

    Added zone checks for boss scans, ficed bug where staticdisplay error handling also incorrectly handles invite popups

--- WorldBossScan.lua
+++ WorldBossScan.lua
@@ -1,11 +1,52 @@
 local WorldBosses = {
-    ["Azuregos"] = 6109,
-    ["Lord Kazzak"] = 12397,
-    ["Emeriss"] = 14889,
-    ["Lethon"] = 14888,
-    ["Taerar"] = 14890,
-    ["Ysondre"] = 14887,
-    ["Undertaker Mordo"] = 1666  -- Test NPC
+    ["Azuregos"] = {
+        id = 6109,
+        zones = { [1445] = true } -- Azshara
+    },
+    ["Lord Kazzak"] = {
+        id = 12397,
+        zones = { [1419] = true } -- Blasted Lands
+    },
+    ["Emeriss"] = {
+        id = 14889,
+        zones = { 
+            [1447] = true, -- Duskwood
+            [1438] = true, -- Ashenvale
+            [1444] = true, -- Feralas
+            [1423] = true  -- The Hinterlands
+        }
+    },
+    ["Lethon"] = {
+        id = 14888,
+        zones = { 
+            [1447] = true, -- Duskwood
+            [1438] = true, -- Ashenvale
+            [1444] = true, -- Feralas
+            [1423] = true  -- The Hinterlands
+        }
+    },
+    ["Taerar"] = {
+        id = 14890,
+        zones = { 
+            [1447] = true, -- Duskwood
+            [1438] = true, -- Ashenvale
+            [1444] = true, -- Feralas
+            [1423] = true  -- The Hinterlands
+        }
+    },
+    ["Ysondre"] = {
+        id = 14887,
+        zones = { 
+            [1447] = true, -- Duskwood
+            [1438] = true, -- Ashenvale
+            [1444] = true, -- Feralas
+            [1423] = true  -- The Hinterlands
+        }
+    },
+    ["Undertaker Mordo"] = {
+        id = 1666,
+        zones = { [1420] = true } -- Tirisfal Glades
+    }
 }
 
 local BossYells = {
@@ -52,6 +93,7 @@ local BossYells = {
 }
 
 local foundBossLayers = {}
+local isLoggingOut = false
 WorldBossScanDB = WorldBossScanDB or {}
 WorldBossScanDB.buttonPosition = WorldBossScanDB.buttonPosition or { x = 0, y = -50 } -- Default top center
 
@@ -59,19 +101,18 @@ WorldBossScanDB.buttonPosition = WorldBossScanDB.buttonPosition or { x = 0, y = 
 local frame = CreateFrame("Frame", "WorldBossScanFrame", UIParent)
 
 -- Create alert button
-local scanner_button = CreateFrame("Button", "WorldBossScanButton", UIParent, "SecureActionButtonTemplate, BackdropTemplate")
+local scanner_button = CreateFrame("Button", "WorldBossScanButton", UIParent, "BackdropTemplate")
 scanner_button:Hide()
 scanner_button:SetIgnoreParentScale(true)
 scanner_button:SetFrameStrata("MEDIUM")
 scanner_button:SetFrameLevel(200)
 scanner_button:SetSize(200, 50)
 scanner_button:SetScale(0.8)
-scanner_button:RegisterForClicks("RightButtonUp", "LeftButtonUp")
-scanner_button:SetAttribute("*type1", "macro")
-scanner_button:SetAttribute("*type2", "closebutton")
+scanner_button:RegisterForClicks("AnyUp")
 scanner_button:SetNormalTexture([[Interface\AchievementFrame\UI-Achievement-Parchment-Horizontal-Desaturated]])
 scanner_button:SetMovable(true)
 scanner_button:EnableMouse(true)
+
 scanner_button:RegisterForDrag("LeftButton")
 if WorldBossScanDB.buttonPosition then
     scanner_button:SetPoint("TOP", UIParent, "TOP", WorldBossScanDB.buttonPosition.x, WorldBossScanDB.buttonPosition.y)
@@ -145,8 +186,8 @@ local function CreateInviteButton(parentButton, playerName, bossName)
         parentButton.inviteButton:Hide()
     end
     
-    local inviteButton = CreateFrame("Button", nil, parentButton, "UIPanelButtonTemplate")
-    inviteButton:SetPoint("TOP", parentButton, "TOP", 0, -20)
+    local inviteButton = CreateFrame("Button", nil, parentButton, "GameMenuButtonTemplate")
+    inviteButton:SetPoint("TOP", parentButton, "Center", 0, 0)
     inviteButton:SetSize(100, 25)
     inviteButton:SetText("Request Invite")
     inviteButton:SetScript("OnClick", function()
@@ -162,10 +203,10 @@ end
 local function ShowAlert(bossName, finderName)
     local currentLayer = GetCurrentLayer()
     if not currentLayer then return end
-    
+    scanner_button.bossName = bossName
     local bossLayerKey = bossName.."-"..currentLayer
 
-    if foundBossLayers[bossLayerKey] and (GetTime() - foundBossLayers[bossLayerKey]) < (4 * 3600) then -- 4 hours
+    if foundBossLayers[bossLayerKey] and (GetTime() - foundBossLayers[bossLayerKey]) < (4 * 3600) then
         return
     end
     
@@ -176,14 +217,18 @@ local function ShowAlert(bossName, finderName)
             C_ChatInfo.SendAddonMessage("WorldBossScan", message, "GUILD")
         end
         finderName = UnitName("player")
+        
+        if UnitExists("target") and UnitName("target") == bossName then
+            SetRaidTarget("target", 8)
+        end
     end
 
     if finderName == UnitName("player") then
         scanner_button.text:SetText(bossName.." found!")
-        local macrotext = "/cleartarget\n/targetexact "..bossName
-        scanner_button:SetAttribute("macrotext", macrotext)
+        scanner_button.bossName = bossName
     else
         scanner_button.text:SetText(finderName.." found "..bossName.."!")
+        scanner_button.bossName = bossName
         CreateInviteButton(scanner_button, finderName, bossName)
     end
     
@@ -207,38 +252,45 @@ end
 
 -- Close error popups
 local function CloseErrorPopUp()
-    if (StaticPopup_HasDisplayedFrames()) then
-        for idx = STATICPOPUP_NUMDIALOGS,1,-1 do
-            local dialog = _G["StaticPopup"..idx]
-            local OnCancel = dialog.OnCancel
-            local noCancelOnEscape = dialog.noCancelOnEscape
-            if (OnCancel and not noCancelOnEscape) then
-                OnCancel(dialog)
+    if StaticPopup_HasDisplayedFrames() then
+        for idx = STATICPOPUP_NUMDIALOGS, 1, -1 do
+            local dialog = _G["StaticPopup" .. idx]
+            if dialog and dialog:IsShown() and dialog.which ~= "PARTY_INVITE" then
+                local OnCancel = dialog.OnCancel
+                local noCancelOnEscape = dialog.noCancelOnEscape
+                if OnCancel and not noCancelOnEscape then
+                    OnCancel(dialog)
+                end
+                StaticPopupSpecial_Hide(dialog)
             end
-            StaticPopupSpecial_Hide(dialog)
         end
     end
 end
 
 -- Scanning function 
 local checking = false
+
 local function ScanForBosses()
-    if (checking) then
+    if (checking or isLoggingOut) then
         return
     end
-    
     checking = true
-    
-    for bossName, _ in pairs(WorldBosses) do
-        TargetUnit(bossName)
-        
-        if (npcFound) then
-            CloseErrorPopUp()
-            ShowAlert(bossName) 
-            npcFound = false
+    local currentZone = C_Map.GetBestMapForUnit("player")
+    if not currentZone then
+        checking = false
+        return
+    end
+    for bossName, bossInfo in pairs(WorldBosses) do
+        if bossInfo.zones[currentZone] then
+            TargetUnit(bossName)
+            
+            if (npcFound) then
+                CloseErrorPopUp()
+                ShowAlert(bossName)
+                npcFound = false
+            end
         end
     end
-    
     checking = false
 end
 
@@ -250,6 +302,8 @@ frame:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 frame:RegisterEvent("ADDON_ACTION_FORBIDDEN")
 frame:RegisterEvent("CHAT_MSG_ADDON")
 frame:RegisterEvent("CHAT_MSG_MONSTER_YELL")
+frame:RegisterEvent("PLAYER_LEAVING_WORLD")
+frame:RegisterEvent("PLAYER_LOGOUT")
 
 -- Register addon prefix
 C_ChatInfo.RegisterAddonMessagePrefix("WorldBossScan")
@@ -257,14 +311,11 @@ C_ChatInfo.RegisterAddonMessagePrefix("WorldBossScan")
 -- Event handler
 frame:SetScript("OnEvent", function(self, event, ...)
     if event == "ADDON_ACTION_FORBIDDEN" then
-        OnAddonActionForbidden(...)
-        
+        OnAddonActionForbidden(...)     
     elseif event == "PLAYER_LOGIN" or event == "PLAYER_ENTERING_WORLD" then
-        C_Timer.After(1, ScanForBosses)
-        
+        C_Timer.After(1, ScanForBosses)     
     elseif event == "ZONE_CHANGED" or event == "ZONE_CHANGED_NEW_AREA" then
-        C_Timer.After(1, ScanForBosses)
-        
+        C_Timer.After(1, ScanForBosses)    
     elseif event == "CHAT_MSG_ADDON" then
         local prefix, message, channel, sender = ...
         if prefix == "WorldBossScan" and channel == "GUILD" then
@@ -276,8 +327,7 @@ frame:SetScript("OnEvent", function(self, event, ...)
                 foundBossLayers[bossLayerKey] = GetTime()
                 ShowAlert(bossName, playerName)
             end
-        end
-        
+        end   
     elseif event == "CHAT_MSG_MONSTER_YELL" then
         local message, monsterName = ...
         for bossName, yells in pairs(BossYells) do
@@ -295,13 +345,22 @@ frame:SetScript("OnEvent", function(self, event, ...)
                     end
                 end
             end
-        end
+        end    
+    elseif event == "PLAYER_LEAVING_WORLD" or event == "PLAYER_LOGOUT" then
+        isLoggingOut = true  
+    elseif event == "LOGOUT_CANCELED" then
+        isLoggingOut = false
     end
 end)
 
 -- Button scripts
 scanner_button:SetScript("OnClick", function(self, button)
-    if button == "RightButton" then
+    if button == "LeftButton" and self.bossName then
+        TargetUnit(self.bossName)
+        if UnitExists("target") then
+            SetRaidTarget("target", 8)  -- Set skull marker
+        end
+    elseif button == "RightButton" then
         self:Hide()
         if self.inviteButton then
             self.inviteButton:Hide()
