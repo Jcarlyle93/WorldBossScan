822843346d9ffe79a18dfa0bc9c71e50cf5ae671
jcarlyle93 <j.j.carlyle93@gmail.com>
2025-01-20 11:12:55 +0000

    Map IDs to replace zoneIDs

--- WorldBossList.lua
+++ WorldBossList.lua
@@ -1,46 +1,46 @@
 WorldBosses = {
     ["Azuregos"] = {
         id = 6109,
-        zones = { [16] = true } -- Azshara
+        zones = { [1447] = true } -- Azshara
     },
     ["Lord Kazzak"] = {
         id = 12397,
-        zones = { [4] = true } -- Blasted Lands
+        zones = { [1419] = true } -- Blasted Lands
     },
     ["Emeriss"] = {
         id = 14889,
         zones = { 
-            [10] = true, -- Duskwood
-            [331] = true, -- Ashenvale
-            [357] = true, -- Feralas
-            [47] = true  -- The Hinterlands
+            [1431] = true, -- Duskwood
+            [1440] = true, -- Ashenvale
+            [1444] = true, -- Feralas
+            [1425] = true  -- The Hinterlands
         }
     },
     ["Lethon"] = {
         id = 14888,
         zones = { 
-            [10] = true, -- Duskwood
-            [331] = true, -- Ashenvale
-            [357] = true, -- Feralas
-            [47] = true  -- The Hinterlands
+            [1431] = true, -- Duskwood
+            [1440] = true, -- Ashenvale
+            [1444] = true, -- Feralas
+            [1425] = true  -- The Hinterlands
         }
     },
     ["Taerar"] = {
         id = 14890,
         zones = { 
-            [10] = true, -- Duskwood
-            [331] = true, -- Ashenvale
-            [357] = true, -- Feralas
-            [47] = true  -- The Hinterlands
+            [1431] = true, -- Duskwood
+            [1440] = true, -- Ashenvale
+            [1444] = true, -- Feralas
+            [1425] = true  -- The Hinterlands
         }
     },
     ["Ysondre"] = {
         id = 14887,
         zones = { 
-            [10] = true, -- Duskwood
-            [331] = true, -- Ashenvale
-            [357] = true, -- Feralas
-            [47] = true  -- The Hinterlands
+            [1431] = true, -- Duskwood
+            [1440] = true, -- Ashenvale
+            [1444] = true, -- Feralas
+            [1425] = true  -- The Hinterlands
         }
     }
 }
